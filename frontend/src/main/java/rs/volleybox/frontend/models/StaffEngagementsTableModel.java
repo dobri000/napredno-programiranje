4c0e694169d346f9b734fd24bb1042d7acab0fb7
dobri000 <dimitrije.dobrijevic@gmail.com>
2023-08-25 21:05:12 +0200

    Added package models

---
+++ frontend/src/main/java/rs/volleybox/frontend/models/StaffEngagementsTableModel.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rs.volleybox.frontend.models;
+
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import rs.volleybox.common_lib.domain.StaffMemberEngagement;
+
+/**
+ *
+ * @author HOME
+ */
+public class StaffEngagementsTableModel extends AbstractTableModel {
+
+    private List<StaffMemberEngagement> engagements;
+
+    public StaffEngagementsTableModel(List<StaffMemberEngagement> engagements) {
+        this.engagements = engagements;
+    }
+    
+    
+    
+    @Override
+    public int getRowCount() {
+        return engagements.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 2;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        StaffMemberEngagement engagement = engagements.get(rowIndex);
+        switch(columnIndex){
+            case 0:
+                return engagement.getRoster().getSeason().toString();
+            case 1:
+                return engagement.getRoster().getTeam().getTeamName();
+        }
+        return null;
+    }
+    
+    @Override
+    public String getColumnName(int column) {
+        String[] titles = {"Season", "Team"};
+        return titles[column];
+    }
+    
+    public void removeEngagement(int row){
+        engagements.remove(row);
+        fireTableDataChanged();
+    }
+    
+    public StaffMemberEngagement getEngagement(int row){
+        return engagements.get(row);
+    }
+    
+}
