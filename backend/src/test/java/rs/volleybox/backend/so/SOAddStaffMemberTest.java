c92074cbc307f2d1e80338bd2477667a6ad77689
dobri000 <dimitrije.dobrijevic@gmail.com>
2023-08-28 18:31:58 +0200

    Added tests for SOAddStaffMemberEngagement

---
+++ backend/src/test/java/rs/volleybox/backend/so/SOAddStaffMemberTest.java
@@ -0,0 +1,58 @@
+package rs.volleybox.backend.so;
+
+import org.junit.jupiter.api.Test;
+import rs.volleybox.backend.so.impl.SOAddPlayerEngagement;
+import rs.volleybox.backend.so.impl.SOAddStaffMemberEngagement;
+import rs.volleybox.common_lib.domain.*;
+import rs.volleybox.common_lib.enumeration.Hand;
+import rs.volleybox.common_lib.enumeration.PlayerPosition;
+import rs.volleybox.common_lib.enumeration.ServerResponse;
+import rs.volleybox.common_lib.enumeration.StaffMemberPosition;
+import rs.volleybox.common_lib.transfer.Response;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+class SOAddStaffMemberTest extends SOClassTest {
+
+    @Override
+    protected void createSO() {
+        so = new SOAddStaffMemberEngagement();
+    }
+
+    @Test
+    void testExecuteSuccess() throws Exception {
+        doNothing().when(dbb).addStaffMemberEngagement(any(StaffMemberEngagement.class));
+
+        StaffMember staffMember = new StaffMember(1, "Luka", "Ratkovic", LocalDate.of(2003, 3, 31), new Country(1, "Serbia"));
+        Roster roster = new Roster(new Team(1, "Crvena Zvezda Belgrade", 1945, new Country(1, "Serbia"), new Hall(1, "USC Vozdovac", "Crnotravska 4, Beograd")), 1, new Season(1, 2022, 2023));
+        StaffMemberEngagement staffMemberEngagement = new StaffMemberEngagement(staffMember, roster, StaffMemberPosition.STATISTICIAN);
+        Response response = so.execute(staffMemberEngagement);
+
+        verify(dbb, times(1)).addStaffMemberEngagement(any(StaffMemberEngagement.class));
+
+        assertEquals(ServerResponse.OK, response.getServerResponse());
+        assertNull(response.getObject());
+    }
+
+    @Test
+    void testExecuteError() throws Exception {
+        doThrow(new SQLException()).when(dbb).addStaffMemberEngagement(any(StaffMemberEngagement.class));
+
+        StaffMember staffMember = new StaffMember(1, "Luka", "Ratkovic", LocalDate.of(2003, 3, 31), new Country(1, "Serbia"));
+        Roster roster = new Roster(new Team(1, "Crvena Zvezda Belgrade", 1945, new Country(1, "Serbia"), new Hall(1, "USC Vozdovac", "Crnotravska 4, Beograd")), 1, new Season(1, 2022, 2023));
+        StaffMemberEngagement staffMemberEngagement = new StaffMemberEngagement(staffMember, roster, StaffMemberPosition.STATISTICIAN);
+        Response response = so.execute(staffMemberEngagement);
+
+        verify(dbb, times(1)).addStaffMemberEngagement(any(StaffMemberEngagement.class));
+
+        assertEquals(ServerResponse.ERROR, response.getServerResponse());
+        assertEquals("Staff member engagement not successfully added", (String) response.getObject());
+    }
+
+}
