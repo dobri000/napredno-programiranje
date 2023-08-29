4c8c158da93ca85bb0cfb232ec4cc91d53183c74
dobri000 <dimitrije.dobrijevic@gmail.com>
2023-08-29 14:10:26 +0200

    Added javadoc for SOSearchPlayers

--- backend/src/main/java/rs/volleybox/backend/so/impl/SOSearchPlayers.java
+++ backend/src/main/java/rs/volleybox/backend/so/impl/SOSearchPlayers.java
@@ -18,21 +18,33 @@ import rs.volleybox.common_lib.transfer.Response;
 import rs.volleybox.common_lib.utils.JsonSerializationUtils;
 
 /**
+ * This class represents a system operation for searching players based on a search term.
+ * The system operation searches for players whose first names or last names contain or closely match the provided search term.
+ *
+ * The search term is used to find players with first names or last names that contain or closely match the search text.
  *
  * @author HOME
  */
 public class SOSearchPlayers extends SOClass {
 
+    /**
+     * Executes the system operation to search for players based on the provided search term.
+     *
+     * @param object The search term (as a String) used to search for players.
+     * @return A {@link Response} containing the list of {@link Player} objects retrieved from the database.
+     *         If the operation is successful, the response will have status {@link ServerResponse#OK} and the list of players.
+     *         If an error occurs during the database operation, the response will have status {@link ServerResponse#ERROR}
+     *         and the error message will provide more information.
+     * @throws IOException If there is an issue with input/output during JSON conversion.
+     */
     @Override
     public Response execute(Object object) throws IOException {
         try {
-            String search = JsonSerializationUtils.convertValue(object, new TypeReference<String>() {
-			});
+            String search = JsonSerializationUtils.convertValue(object, new TypeReference<String>() {});
             List<Player> players = dbbroker.getPlayersByFirstnameOrLastname(search);
             return new Response(ServerResponse.OK, players);
         } catch (SQLException ex) {
-            return new Response(ServerResponse.ERROR, "Cannot return players for by given name");
+            return new Response(ServerResponse.ERROR, "Cannot return players by the given name");
         }
     }
-    
 }
