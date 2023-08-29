faab05364f144e05311abb8b444658d714f616fa
dobri000 <dimitrije.dobrijevic@gmail.com>
2023-08-29 14:11:16 +0200

    Added javadoc for SOUpdateStaffMember

--- backend/src/main/java/rs/volleybox/backend/so/impl/SOUpdateStaffMember.java
+++ backend/src/main/java/rs/volleybox/backend/so/impl/SOUpdateStaffMember.java
@@ -17,21 +17,32 @@ import rs.volleybox.common_lib.transfer.Response;
 import rs.volleybox.common_lib.utils.JsonSerializationUtils;
 
 /**
+ * This class represents a system operation for updating staff member information in the database.
+ * The system operation updates the information of the provided staff member object in the database.
  *
  * @author HOME
  */
 public class SOUpdateStaffMember extends SOClass {
 
+    /**
+     * Executes the system operation to update staff member information in the database.
+     *
+     * @param object The staff member object with updated information.
+     * @return A response indicating the outcome of the update operation.
+     *         If the operation is successful, the response will have status {@link ServerResponse#OK}.
+     *         If an error occurs during the database operation, the response will have status {@link ServerResponse#ERROR}
+     *         and the error message will provide more information.
+     * @throws IOException If there is an issue with input/output during JSON conversion.
+     */
     @Override
     public Response execute(Object object) throws IOException {
         try {
-            StaffMember staff = JsonSerializationUtils.convertValue(object, new TypeReference<StaffMember>() {
-			});
+            StaffMember staff = JsonSerializationUtils.convertValue(object, new TypeReference<StaffMember>() {});
             dbbroker.updateStaffMember(staff);
             return new Response(ServerResponse.OK, null);
         } catch (SQLException ex) {
-            return new Response(ServerResponse.ERROR, "Cannot update staff member");
+            return new Response(ServerResponse.ERROR, "Cannot update staff member information");
         }
     }
-    
+
 }
