e4cd0aff57e59340de49e21605c0e7f42598d616
dobri000 <dimitrije.dobrijevic@gmail.com>
2023-08-29 14:04:14 +0200

    Added javadoc for SOAddStaffMember

--- backend/src/main/java/rs/volleybox/backend/so/impl/SOAddStaffMember.java
+++ backend/src/main/java/rs/volleybox/backend/so/impl/SOAddStaffMember.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rs.volleybox.backend.so.impl;
 
 import java.io.IOException;
@@ -11,23 +7,35 @@ import com.fasterxml.jackson.core.type.TypeReference;
 
 import rs.volleybox.backend.database.DBBroker;
 import rs.volleybox.backend.so.SOClass;
-import rs.volleybox.common_lib.domain.Player;
 import rs.volleybox.common_lib.domain.StaffMember;
 import rs.volleybox.common_lib.enumeration.ServerResponse;
 import rs.volleybox.common_lib.transfer.Response;
 import rs.volleybox.common_lib.utils.JsonSerializationUtils;
 
 /**
+ * This class represents a system operation for adding a new staff member to the database.
+ * The staff member details are provided in the input object, and the system operation
+ * handles the process of adding the staff member to the database using the provided DBBroker.
  *
  * @author HOME
  */
 public class SOAddStaffMember extends SOClass {
-    
+
+    /**
+     * Executes the system operation to add a new staff member to the database.
+     *
+     * @param object The input object containing the details of the staff member to be added.
+     * @return A {@link Response} indicating the result of the operation.
+     *         If the staff member is successfully added, the response will have status {@link ServerResponse#OK}.
+     *         If an error occurs during the database operation, the response will have status {@link ServerResponse#ERROR}
+     *         and the error message will provide more information.
+     * @throws IOException If there is an issue with input/output during JSON conversion.
+     */
     @Override
     public Response execute(Object object) throws IOException {
         try {
             StaffMember staff = JsonSerializationUtils.convertValue(object, new TypeReference<StaffMember>() {
-			});
+            });
             dbbroker.addStaffMember(staff);
             return new Response(ServerResponse.OK, null);
         } catch (SQLException ex) {
@@ -35,6 +43,4 @@ public class SOAddStaffMember extends SOClass {
         }
     }
 
-    
-    
 }
