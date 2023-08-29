49e9876d1eab18ebcfc9a79364fa6b32afa84d94
dobri000 <dimitrije.dobrijevic@gmail.com>
2023-08-29 14:03:11 +0200

    Added javadoc for SOClass

--- backend/src/main/java/rs/volleybox/backend/so/SOClass.java
+++ backend/src/main/java/rs/volleybox/backend/so/SOClass.java
@@ -1,31 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rs.volleybox.backend.so;
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.sql.SQLException;
 
 import rs.volleybox.backend.database.DBBroker;
 import rs.volleybox.common_lib.transfer.Response;
 
 /**
+ * Abstract class defining the basic structure for all system operation classes.
+ *
+ * All concrete classes that inherit from this class must implement the execute method,
+ * which represents the execution of a specific system operation.
+ *
+ * Additionally, this class allows setting instances of the DBBroker to be used
+ * in the execution of system operations.
  *
  * @author HOME
  */
 public abstract class SOClass {
-    
-	protected DBBroker dbbroker;
-	
-    public abstract Response execute(Object object) throws IOException;
 
+	/**
+	 * Reference to the DBBroker instance to be used during system operation execution.
+	 */
+	protected DBBroker dbbroker;
+
+	/**
+	 * Abstract method defining the execution of a specific system operation.
+	 *
+	 * @param object The object used for executing the operation.
+	 * @return The result of the operation execution as a Response object.
+	 * @throws IOException In case of communication errors.
+	 */
+	public abstract Response execute(Object object) throws IOException;
+
+	/**
+	 * Sets the DBBroker instance to be used during system operation execution.
+	 *
+	 * @param dbbroker A reference to the DBBroker instance.
+	 */
 	public void setDbbroker(DBBroker dbbroker) {
 		this.dbbroker = dbbroker;
 	}
-    
-    
-    
 }
