e3adb805e9d7cf0294ec006de0e474dbc52d92a4
dobri000 <dimitrije.dobrijevic@gmail.com>
2023-08-29 14:10:36 +0200

    Added javadoc for SOSearchStaffMembers

--- backend/src/main/java/rs/volleybox/backend/so/impl/SOSearchStaffMembers.java
+++ backend/src/main/java/rs/volleybox/backend/so/impl/SOSearchStaffMembers.java
@@ -18,21 +18,34 @@ import rs.volleybox.common_lib.transfer.Response;
 import rs.volleybox.common_lib.utils.JsonSerializationUtils;
 
 /**
+ * This class represents a system operation for searching staff members based on a search term.
+ * The system operation searches for staff members whose first names or last names contain or closely match the provided search term.
+ *
+ * The search term is used to find staff members with first names or last names that contain or closely match the search text.
  *
  * @author HOME
  */
 public class SOSearchStaffMembers extends SOClass {
 
+    /**
+     * Executes the system operation to search for staff members based on the provided search term.
+     *
+     * @param object The search term (as a String) used to search for staff members.
+     * @return A {@link Response} containing the list of {@link StaffMember} objects retrieved from the database.
+     *         If the operation is successful, the response will have status {@link ServerResponse#OK} and the list of staff members.
+     *         If an error occurs during the database operation, the response will have status {@link ServerResponse#ERROR}
+     *         and the error message will provide more information.
+     * @throws IOException If there is an issue with input/output during JSON conversion.
+     */
     @Override
     public Response execute(Object object) throws IOException {
         try {
-            String search = JsonSerializationUtils.convertValue(object, new TypeReference<String>() {
-			});
+            String search = JsonSerializationUtils.convertValue(object, new TypeReference<String>() {});
             List<StaffMember> staffMembers = dbbroker.getStaffMembersByFirstnameOrLastname(search);
             return new Response(ServerResponse.OK, staffMembers);
         } catch (SQLException ex) {
-            return new Response(ServerResponse.ERROR, "Cannot return staff members for by given name");
+            return new Response(ServerResponse.ERROR, "Cannot return staff members by the given name");
         }
     }
-    
 }
+
