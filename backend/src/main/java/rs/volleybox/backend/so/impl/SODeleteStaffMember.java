bd841a8a5d6f0df941041f7bbf628b8e9c6281d0
dobri000 <dimitrije.dobrijevic@gmail.com>
2023-08-29 14:05:11 +0200

    Added javadoc for SODeleteStaffMember

--- backend/src/main/java/rs/volleybox/backend/so/impl/SODeleteStaffMember.java
+++ backend/src/main/java/rs/volleybox/backend/so/impl/SODeleteStaffMember.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rs.volleybox.backend.so.impl;
 
 import java.io.IOException;
@@ -11,28 +7,40 @@ import com.fasterxml.jackson.core.type.TypeReference;
 
 import rs.volleybox.backend.database.DBBroker;
 import rs.volleybox.backend.so.SOClass;
-import rs.volleybox.common_lib.domain.Player;
 import rs.volleybox.common_lib.domain.StaffMember;
 import rs.volleybox.common_lib.enumeration.ServerResponse;
 import rs.volleybox.common_lib.transfer.Response;
 import rs.volleybox.common_lib.utils.JsonSerializationUtils;
 
 /**
+ * This class represents a system operation for deleting a staff member from the database.
+ * The staff member to be deleted is specified in the input object, and the system operation
+ * handles the process of removing the staff member from the database using the provided DBBroker.
  *
  * @author HOME
  */
 public class SODeleteStaffMember extends SOClass {
 
+    /**
+     * Executes the system operation to delete a staff member from the database.
+     *
+     * @param object The input object specifying the staff member to be deleted.
+     * @return A {@link Response} indicating the result of the operation.
+     *         If the staff member is successfully deleted, the response will have status {@link ServerResponse#OK}.
+     *         If an error occurs during the database operation, the response will have status {@link ServerResponse#ERROR}
+     *         and the error message will provide more information.
+     * @throws IOException If there is an issue with input/output during JSON conversion.
+     */
     @Override
     public Response execute(Object object) throws IOException {
         try {
             StaffMember staff = JsonSerializationUtils.convertValue(object, new TypeReference<StaffMember>() {
-			});
+            });
             dbbroker.deleteStaffMember(staff);
             return new Response(ServerResponse.OK, null);
         } catch (SQLException ex) {
             return new Response(ServerResponse.ERROR, "Staff member not successfully deleted");
         }
     }
-    
+
 }
