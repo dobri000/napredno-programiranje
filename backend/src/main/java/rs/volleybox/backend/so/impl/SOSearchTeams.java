96606ebc099f76a4937bc6ca27a9267e01fe8771
dobri000 <dimitrije.dobrijevic@gmail.com>
2023-08-29 14:10:51 +0200

    Added javadoc for SOSearchTeams

--- backend/src/main/java/rs/volleybox/backend/so/impl/SOSearchTeams.java
+++ backend/src/main/java/rs/volleybox/backend/so/impl/SOSearchTeams.java
@@ -18,23 +18,34 @@ import rs.volleybox.common_lib.transfer.Response;
 import rs.volleybox.common_lib.utils.JsonSerializationUtils;
 
 /**
+ * This class represents a system operation for searching teams based on a search term.
+ * The system operation searches for teams whose names or codes contain or closely match the provided search term.
+ *
+ * The search term is used to find teams with names or codes that contain or closely match the search text.
  *
  * @author HOME
  */
 public class SOSearchTeams extends SOClass {
 
+    /**
+     * Executes the system operation to search for teams based on the provided search term.
+     *
+     * @param object The search term (as a String) used to search for teams.
+     * @return A {@link Response} containing the list of {@link Team} objects retrieved from the database.
+     *         If the operation is successful, the response will have status {@link ServerResponse#OK} and the list of teams.
+     *         If an error occurs during the database operation, the response will have status {@link ServerResponse#ERROR}
+     *         and the error message will provide more information.
+     * @throws IOException If there is an issue with input/output during JSON conversion.
+     */
     @Override
     public Response execute(Object object) throws IOException {
         try {
-            String search = JsonSerializationUtils.convertValue(object, new TypeReference<String>() {
-			});
+            String search = JsonSerializationUtils.convertValue(object, new TypeReference<String>() {});
             List<Team> teams = dbbroker.searchTeam(search);
             return new Response(ServerResponse.OK, teams);
         } catch (SQLException ex) {
-            return new Response(ServerResponse.ERROR, "Cannot return teams for by given name");
+            return new Response(ServerResponse.ERROR, "Cannot return teams by the given name");
         }
     }
-    
-    
-    
 }
+
